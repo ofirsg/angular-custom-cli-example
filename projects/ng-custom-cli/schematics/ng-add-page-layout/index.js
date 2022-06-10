67ee0229a6975833164af1bcb128e9751e5d3a24
ofirsg <ofirsg@gmail.com>
2022-06-10 16:12:32 +0300

    fix template

--- projects/ng-custom-cli/schematics/ng-add-page-layout/index.js
+++ projects/ng-custom-cli/schematics/ng-add-page-layout/index.js
@@ -5,33 +5,55 @@ const schematics_1 = require("@angular-devkit/schematics");
 const core_1 = require("@angular-devkit/core");
 function pageLayoutComponentGenerator(options) {
     return (tree) => {
-        var _a;
-        if (!options.path || !options.name) {
+        if (!options.name) {
             console.log('---------------------------------------------------');
-            console.log('Missing options!');
+            console.log('Missing name!');
             console.log('---------------------------------------------------');
             return;
         }
-        // const bla = tree.exists(options.path) ? tree.get(options.path) : null;
-        const bla = tree.exists(options.path);
-        console.log('################################################################');
-        console.debug((_a = tree.getDir('projects')) === null || _a === void 0 ? void 0 : _a.subdirs);
-        // console.log(bla?.content?.toString());
-        console.log(bla);
-        console.log('################################################################');
+        if (!options.path || !tree.exists(options.path)) {
+            console.log('---------------------------------------------------');
+            console.log('Invalid path!');
+            console.log('---------------------------------------------------');
+            return;
+        }
+        const pathParts = options.path.split('/');
+        const newFolderPath = pathParts.slice(0, pathParts.length - 2).join('/');
         const templateSource = (0, schematics_1.apply)((0, schematics_1.url)('./files'), [
             (0, schematics_1.applyTemplates)({
                 classify: core_1.strings.classify,
                 dasherize: core_1.strings.dasherize,
                 name: options.name
             }),
-            (0, schematics_1.move)((0, core_1.normalize)(`/${options.path}/page-layout/${core_1.strings.dasherize(options.name)}`))
+            (0, schematics_1.move)((0, core_1.normalize)(`/${newFolderPath}/page-layout/${core_1.strings.dasherize(options.name)}`))
         ]);
-        // TODO: add the new component by path
         return (0, schematics_1.chain)([
-            (0, schematics_1.mergeWith)(templateSource, schematics_1.MergeStrategy.Overwrite)
+            (0, schematics_1.mergeWith)(templateSource, schematics_1.MergeStrategy.Overwrite),
+            updateHtmlFile,
         ]);
     };
+    function updateHtmlFile() {
+        return (tree, _context) => {
+            var _a, _b, _c, _d;
+            const htmlContent = (_b = (_a = tree.get(options.path)) === null || _a === void 0 ? void 0 : _a.content.toString()) !== null && _b !== void 0 ? _b : '';
+            const componentSelector = core_1.strings.dasherize(options.name);
+            const newHtmlContent = `
+<${componentSelector} [pageContext]="pageContext">
+  ${htmlContent}
+</${componentSelector}>
+`;
+            const compFilePath = options.path.replace('html', 'ts');
+            const compContent = (_d = (_c = tree.get(compFilePath)) === null || _c === void 0 ? void 0 : _c.content.toString()) !== null && _d !== void 0 ? _d : '';
+            // can be done better with ast helpers such ts-morph
+            const start = compContent.indexOf('{', compContent.indexOf('class'));
+            const newCompContent = `${compContent.substring(0, start)}
+   public pageContext = 'pageContext'; // TODO: fix
+  ${compContent.substring(start)}`;
+            tree.overwrite(compFilePath, newCompContent);
+            tree.overwrite(options.path, newHtmlContent);
+            return tree;
+        };
+    }
 }
 exports.pageLayoutComponentGenerator = pageLayoutComponentGenerator;
 //# sourceMappingURL=index.js.map
--- projects/ng-custom-cli/schematics/ng-add-page-layout/index.ts
+++ projects/ng-custom-cli/schematics/ng-add-page-layout/index.ts
@@ -5,7 +5,7 @@ import {
   MergeStrategy,
   mergeWith,
   move,
-  Rule, Tree,
+  Rule, SchematicContext, Tree,
   url
 } from "@angular-devkit/schematics";
 import { normalize, strings } from "@angular-devkit/core";
@@ -13,20 +13,22 @@ import { PageLayoutComponentSchema } from "./page-layout.interfaces";
 
 export function pageLayoutComponentGenerator(options: PageLayoutComponentSchema): Rule {
   return (tree: Tree) => {
-    if(!options.path || !options.name){
+    if(!options.name){
       console.log('---------------------------------------------------');
-      console.log('Missing options!');
+      console.log('Missing name!');
       console.log('---------------------------------------------------');
       return;
     }
 
-    // const bla = tree.exists(options.path) ? tree.get(options.path) : null;
-    const bla = tree.exists(options.path);
-    console.log('################################################################');
-    console.debug(tree.getDir('projects')?.subdirs);
-    // console.log(bla?.content?.toString());
-    console.log(bla)
-    console.log('################################################################');
+    if(!options.path || !tree.exists(options.path)){
+      console.log('---------------------------------------------------');
+      console.log('Invalid path!');
+      console.log('---------------------------------------------------');
+      return;
+    }
+
+    const pathParts: string[] = options.path.split('/');
+    const newFolderPath: string = pathParts.slice(0, pathParts.length - 1).join('/');
 
     const templateSource = apply(
       url('./files'), [
@@ -35,14 +37,40 @@ export function pageLayoutComponentGenerator(options: PageLayoutComponentSchema)
           dasherize: strings.dasherize,
           name: options.name
         }),
-        move(normalize(`/${options.path}/page-layout/${strings.dasherize(options.name)}`))
+        move(normalize(`/${newFolderPath}/${strings.dasherize(options.name)}`))
       ]
     );
 
-    // TODO: add the new component by path
-
     return chain([
-      mergeWith(templateSource, MergeStrategy.Overwrite)
+      mergeWith(templateSource, MergeStrategy.Overwrite),
+      updateHtmlFile,
     ]);
+  }
+
+  function updateHtmlFile(): Rule {
+    return (tree: Tree, _context: SchematicContext) => {
+      const htmlContent: string = tree.get(options.path)?.content.toString() ?? '';
+      const componentSelector = `app-pl-${strings.dasherize(options.name)}`;
+
+      const newHtmlContent = `
+<${componentSelector} [pageContext]="pageContext">
+  ${htmlContent}
+</${componentSelector}>
+`;
+
+      const compFilePath = options.path.replace('html', 'ts');
+      const compContent: string = tree.get(compFilePath)?.content.toString() ?? '';
+
+      // can be done better with ast helpers such ts-morph
+      const start = compContent.indexOf('{', compContent.indexOf('class'));
+      const newCompContent = `${compContent.substring(0, start + 1)}
+   public pageContext = 'pageContext'; // TODO: fix
+  ${compContent.substring(start + 1)}`;
+
+      tree.overwrite(compFilePath, newCompContent);
+      tree.overwrite(options.path, newHtmlContent);
+
+      return tree;
+    };
   }
 }
