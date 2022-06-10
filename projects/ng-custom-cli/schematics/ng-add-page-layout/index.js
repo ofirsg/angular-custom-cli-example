028c869c409a6d8fe9c1004c7f74927987decb25
ofirsg <ofirsg@gmail.com>
2022-06-10 16:48:21 +0300

    fix flow

--- projects/ng-custom-cli/schematics/ng-add-page-layout/index.js
+++ projects/ng-custom-cli/schematics/ng-add-page-layout/index.js
@@ -20,6 +20,56 @@ function pageLayoutComponentGenerator(options) {
         const className = options.name.endsWith('Component') ? options.name : `${options.name}Component`;
         const pathParts = options.path.split('/');
         const newFolderPath = pathParts.slice(0, pathParts.length - 1).join('/');
+        function updateComponentFile(tree) {
+            var _a, _b;
+            const compFilePath = options.path.replace('html', 'ts');
+            const compContent = (_b = (_a = tree.get(compFilePath)) === null || _a === void 0 ? void 0 : _a.content.toString()) !== null && _b !== void 0 ? _b : '';
+            // can be done better with ast helpers such ts-morph
+            const start = compContent.indexOf('{', compContent.indexOf('class') + 1);
+            const newCompContent = `${compContent.substring(0, start + 1)}
+   public pageContext = 'pageContext'; // TODO: fix
+  ${compContent.substring(start + 1)}`;
+            tree.overwrite(compFilePath, newCompContent);
+        }
+        function updateHtmlFile(tree) {
+            var _a, _b;
+            const htmlContent = (_b = (_a = tree.get(options.path)) === null || _a === void 0 ? void 0 : _a.content.toString()) !== null && _b !== void 0 ? _b : '';
+            const componentSelector = `app-pl-${core_1.strings.dasherize(options.name)}`;
+            const newHtmlContent = `
+<${componentSelector} [pageContext]="pageContext">
+  ${htmlContent}
+</${componentSelector}>
+`;
+            tree.overwrite(options.path, newHtmlContent);
+        }
+        function updateModuleFile(tree) {
+            var _a, _b;
+            const pathParts = options.path.split('/');
+            pathParts.pop();
+            let modulePath = null;
+            const importPathParts = [core_1.strings.dasherize(options.name)];
+            while (pathParts.length > 0 && !modulePath) {
+                const subFiles = tree.getDir(pathParts.join('/')).subfiles;
+                const moduleFilePath = subFiles.find(f => f.includes('.module'));
+                if (moduleFilePath) {
+                    modulePath = `${pathParts.join('/')}/${moduleFilePath}`;
+                }
+                else {
+                    importPathParts.unshift(pathParts.pop());
+                }
+            }
+            if (modulePath) {
+                const moduleFileContent = (_b = (_a = tree.get(modulePath)) === null || _a === void 0 ? void 0 : _a.content.toString()) !== null && _b !== void 0 ? _b : '';
+                const moduleStart = moduleFileContent.indexOf('[', moduleFileContent.indexOf('declarations') + 1);
+                const classNameClassified = core_1.strings.classify(className);
+                const newModuleContent = `
+import { ${classNameClassified} } from './${importPathParts.join('/')}/${core_1.strings.dasherize(options.name)}.component';
+${moduleFileContent.substring(0, moduleStart + 1)}
+   ${classNameClassified},
+  ${moduleFileContent.substring(moduleStart + 1)}`;
+                tree.overwrite(modulePath, newModuleContent);
+            }
+        }
         const templateSource = (0, schematics_1.apply)((0, schematics_1.url)('./files'), [
             (0, schematics_1.applyTemplates)({
                 classify: core_1.strings.classify,
@@ -31,53 +81,14 @@ function pageLayoutComponentGenerator(options) {
         ]);
         return (0, schematics_1.chain)([
             (0, schematics_1.mergeWith)(templateSource, schematics_1.MergeStrategy.Overwrite),
-            updateHtmlFile,
+            (tree, _context) => {
+                updateHtmlFile(tree);
+                updateComponentFile(tree);
+                updateModuleFile(tree);
+                return tree;
+            },
         ]);
     };
-    function updateHtmlFile() {
-        return (tree, _context) => {
-            var _a, _b, _c, _d, _e, _f;
-            const htmlContent = (_b = (_a = tree.get(options.path)) === null || _a === void 0 ? void 0 : _a.content.toString()) !== null && _b !== void 0 ? _b : '';
-            const componentSelector = `app-pl-${core_1.strings.dasherize(options.name)}`;
-            const newHtmlContent = `
-<${componentSelector} [pageContext]="pageContext">
-  ${htmlContent}
-</${componentSelector}>
-`;
-            const compFilePath = options.path.replace('html', 'ts');
-            const compContent = (_d = (_c = tree.get(compFilePath)) === null || _c === void 0 ? void 0 : _c.content.toString()) !== null && _d !== void 0 ? _d : '';
-            // can be done better with ast helpers such ts-morph
-            const start = compContent.indexOf('{', compContent.indexOf('class') + 1);
-            const newCompContent = `${compContent.substring(0, start + 1)}
-   public pageContext = 'pageContext'; // TODO: fix
-  ${compContent.substring(start + 1)}`;
-            const pathParts = compFilePath.split('/');
-            pathParts.pop();
-            let modulePath = null;
-            while (pathParts.length > 0 && !modulePath) {
-                const subFiles = tree.getDir(pathParts.join('/')).subfiles;
-                const moduleFilePath = subFiles.find(f => f.includes('.module'));
-                if (moduleFilePath) {
-                    modulePath = moduleFilePath;
-                    console.log('########################################');
-                    console.log(moduleFilePath);
-                    console.log('########################################');
-                }
-            }
-            if (modulePath) {
-                const moduleFileContent = (_f = (_e = tree.get(modulePath)) === null || _e === void 0 ? void 0 : _e.content.toString()) !== null && _f !== void 0 ? _f : '';
-                const moduleStart = moduleFileContent.indexOf('[', compContent.indexOf('declarations') + 1);
-                const className = options.name.endsWith('Component') ? options.name : `${options.name}Component`;
-                const newModuleContent = `${moduleFileContent.substring(0, moduleStart + 1)}
-   ${className},
-  ${moduleFileContent.substring(start + 1)}`;
-                tree.overwrite(modulePath, newModuleContent);
-            }
-            tree.overwrite(compFilePath, newCompContent);
-            tree.overwrite(options.path, newHtmlContent);
-            return tree;
-        };
-    }
 }
 exports.pageLayoutComponentGenerator = pageLayoutComponentGenerator;
 //# sourceMappingURL=index.js.map
--- projects/ng-custom-cli/schematics/ng-add-page-layout/index.ts
+++ projects/ng-custom-cli/schematics/ng-add-page-layout/index.ts
@@ -32,6 +32,61 @@ export function pageLayoutComponentGenerator(options: PageLayoutComponentSchema)
     const pathParts: string[] = options.path.split('/');
     const newFolderPath: string = pathParts.slice(0, pathParts.length - 1).join('/');
 
+    function updateComponentFile(tree: Tree): void {
+      const compFilePath = options.path.replace('html', 'ts');
+      const compContent: string = tree.get(compFilePath)?.content.toString() ?? '';
+
+      // can be done better with ast helpers such ts-morph
+      const start = compContent.indexOf('{', compContent.indexOf('class') + 1);
+      const newCompContent = `${compContent.substring(0, start + 1)}
+   public pageContext = 'pageContext'; // TODO: fix
+  ${compContent.substring(start + 1)}`;
+
+      tree.overwrite(compFilePath, newCompContent);
+    }
+
+    function updateHtmlFile(tree: Tree): void {
+      const htmlContent: string = tree.get(options.path)?.content.toString() ?? '';
+      const componentSelector = `app-pl-${strings.dasherize(options.name)}`;
+
+      const newHtmlContent = `
+<${componentSelector} [pageContext]="pageContext">
+  ${htmlContent}
+</${componentSelector}>
+`;
+      tree.overwrite(options.path, newHtmlContent);
+    }
+
+    function updateModuleFile(tree: Tree): void {
+      const pathParts = options.path.split('/');
+      pathParts.pop();
+      let modulePath: string | null = null;
+      const importPathParts = [strings.dasherize(options.name)];
+
+      while (pathParts.length > 0 && !modulePath) {
+        const subFiles = tree.getDir(pathParts.join('/')).subfiles;
+        const moduleFilePath = subFiles.find(f => f.includes('.module'));
+
+        if (moduleFilePath) {
+          modulePath = `${pathParts.join('/')}/${moduleFilePath}`;
+        } else {
+          importPathParts.unshift(pathParts.pop() as string);
+        }
+      }
+
+      if (modulePath) {
+        const moduleFileContent = tree.get(modulePath)?.content.toString() ?? '';
+        const moduleStart = moduleFileContent.indexOf('[', moduleFileContent.indexOf('declarations') + 1);
+        const classNameClassified = strings.classify(className);
+        const newModuleContent = `
+import { ${classNameClassified} } from './${importPathParts.join('/')}/${strings.dasherize(options.name)}.component';
+${moduleFileContent.substring(0, moduleStart + 1)}
+   ${classNameClassified},
+  ${moduleFileContent.substring(moduleStart + 1)}`;
+        tree.overwrite(modulePath, newModuleContent);
+      }
+    }
+
     const templateSource = apply(
       url('./files'), [
         applyTemplates({
@@ -46,59 +101,13 @@ export function pageLayoutComponentGenerator(options: PageLayoutComponentSchema)
 
     return chain([
       mergeWith(templateSource, MergeStrategy.Overwrite),
-      updateHtmlFile,
+      (tree: Tree, _context: SchematicContext) => {
+        updateHtmlFile(tree);
+        updateComponentFile(tree);
+        updateModuleFile(tree);
+
+        return tree;
+      },
     ]);
-  }
-
-  function updateHtmlFile(): Rule {
-    return (tree: Tree, _context: SchematicContext) => {
-      const htmlContent: string = tree.get(options.path)?.content.toString() ?? '';
-      const componentSelector = `app-pl-${strings.dasherize(options.name)}`;
-
-      const newHtmlContent = `
-<${componentSelector} [pageContext]="pageContext">
-  ${htmlContent}
-</${componentSelector}>
-`;
-
-      const compFilePath = options.path.replace('html', 'ts');
-      const compContent: string = tree.get(compFilePath)?.content.toString() ?? '';
-
-      // can be done better with ast helpers such ts-morph
-      const start = compContent.indexOf('{', compContent.indexOf('class') + 1);
-      const newCompContent = `${compContent.substring(0, start + 1)}
-   public pageContext = 'pageContext'; // TODO: fix
-  ${compContent.substring(start + 1)}`;
-
-      const pathParts = compFilePath.split('/');
-      pathParts.pop();
-      let modulePath: string | null = null;
-
-      while (pathParts.length > 0 && !modulePath) {
-        const subFiles = tree.getDir(pathParts.join('/')).subfiles;
-        const moduleFilePath = subFiles.find(f => f.includes('.module'));
-
-        if (moduleFilePath) {
-          modulePath = moduleFilePath;
-          console.log('########################################');
-          console.log(moduleFilePath);
-          console.log('########################################');
-        }
-      }
-
-      if (modulePath) {
-        const moduleFileContent = tree.get(modulePath)?.content.toString() ?? '';
-        const moduleStart = moduleFileContent.indexOf('[', compContent.indexOf('declarations') + 1);
-        const className = options.name.endsWith('Component') ? options.name : `${options.name}Component`;
-        const newModuleContent = `${moduleFileContent.substring(0, moduleStart + 1)}
-   ${className},
-  ${moduleFileContent.substring(start + 1)}`;
-        tree.overwrite(modulePath, newModuleContent);
-      }
-      tree.overwrite(compFilePath, newCompContent);
-      tree.overwrite(options.path, newHtmlContent);
-
-      return tree;
-    };
   }
 }
